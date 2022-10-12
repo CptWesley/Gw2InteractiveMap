a03ced9e23cec635d19f8a932c3b84069dd546ab
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2022-10-12 20:24:26 +0200

    Improve rendering of tiles that are not yet cached

--- app/src/logic/worldMapRendering.ts
+++ app/src/logic/worldMapRendering.ts
@@ -2,7 +2,13 @@ import { DrawingContext } from '@/react-app-env';
 import { downloadImage } from '@/logic/imageCache';
 import { getMapInfo, getTileSource } from '@/logic/tileService';
 
+const drawCounts = new Map<CanvasRenderingContext2D, number>();
+
 export function drawMap(ctx: DrawingContext) {
+    const oldDrawCount = drawCounts.get(ctx.graphics);
+    const drawCount = oldDrawCount === undefined ? 0 : oldDrawCount + 1;
+    drawCounts.set(ctx.graphics, drawCount);
+
     const mapInfo = getMapInfo(ctx.continent, ctx.floor);
     const realZoom = mapInfo.maxZoom - ctx.zoom;
     const power = Math.pow(2, realZoom);
@@ -23,14 +29,22 @@ export function drawMap(ctx: DrawingContext) {
             const source = getTileSource(ctx.continent, ctx.floor, tileZoom, x, y);
             if (!source) { continue; }
             const imgPromise = downloadImage(source.url);
-            if (!imgPromise.resolved) { continue; }
-            const img = imgPromise.result;
-            if (!img) { continue; }
+
             const dx = mapInfo.tileWidth * x;
             const dy = mapInfo.tileHeight * y;
             const dw = mapInfo.tileWidth;
             const dh = mapInfo.tileHeight;
-            ctx.graphics.drawImage(img, source.x, source.y, source.width, source.height, dx, dy, dw, dh);
+
+            if (imgPromise.resolved) {
+                const img = imgPromise.result;
+                ctx.graphics.drawImage(img, source.x, source.y, source.width, source.height, dx, dy, dw, dh);
+            } else {
+                imgPromise.promise.then((img) => {
+                    if (drawCounts.get(ctx.graphics) === drawCount) {
+                        ctx.graphics.drawImage(img, source.x, source.y, source.width, source.height, dx, dy, dw, dh);
+                    }
+                });
+            }
         }
     }
 }
